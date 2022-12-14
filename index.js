b03412221cf182bbf3ffbc3dc6947119ee942fd7
Lydia Vega <lyvegama@gmail.com>
2022-12-14 11:11:21 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        hello: 'hi',
+    });
+
+});
+
+
+app.listen("3000", () => {
+    console.log('Server is listening  to PORT 3000');
+});
